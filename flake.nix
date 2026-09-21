b893c141fa89f43a9d042b2b55e432e7fb0d69a1
kyanagis <kyanagis42@gmail.com>
2026-09-22 05:40:11 +0900

    chore: align OVA source with validated release baseline

--- flake.nix
+++ flake.nix
@@ -8,24 +8,11 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-
-    home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    plasma-manager = {
-      url = "github:nix-community/plasma-manager/trunk";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
-    };
   };
 
   outputs =
     {
       nixpkgs,
-      home-manager,
-      plasma-manager,
       ...
     }:
     let
@@ -45,7 +32,6 @@
         inherit system;
         modules = [
           baseModule
-          ./nix/desktop.nix
           hostBootModule
         ];
       };
@@ -53,88 +39,6 @@
         inherit system;
         modules = [
           baseModule
-          ./nix/desktop.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              extraSpecialArgs = {
-                inherit userName;
-              };
-              sharedModules = [
-                plasma-manager.homeModules.plasma-manager
-              ];
-              users.${userName} = {
-                imports = [
-                  ./nix/home.nix
-                ];
-
-                home = {
-                  packages = with pkgs; [
-                    bibata-cursors
-                    papirus-nord
-                    plasma-panel-colorizer
-                    (callPackage ./nix/nordic-plasma-theme.nix { })
-                  ];
-
-                  username = userName;
-                  homeDirectory = "/home/${userName}";
-                  stateVersion = "26.05";
-                };
-
-                xdg.configFile."autostart/inception-setup.desktop".text = ''
-                  [Desktop Entry]
-                  Type=Application
-                  Name=Configure Inception 42 Login
-                  Comment=Configure /home/<login>/data and <login>.42.fr
-                  Exec=/run/current-system/sw/bin/inception-setup --first-login
-                  Terminal=false
-                  X-KDE-autostart-after=panel
-                '';
-
-                home.file."Desktop/INCEPTION-SETUP.txt".text = ''
-                  Inception VM 利用手順
-                  =====================
-
-                  共通のOSアカウント名は inception、初期パスワードも inception です。
-
-                  初回ログイン時に表示される設定画面へ、自分の42ログインを入力してください。
-                  設定画面を閉じた場合や、あとから変更する場合は次を実行できます。
-
-                      inception-setup YOUR_42_LOGIN
-
-                  これにより、次のパス・ドメイン・環境変数が設定されます。
-
-                    /home/YOUR_42_LOGIN/data
-                    YOUR_42_LOGIN.42.fr
-                    INCEPTION_LOGIN, DOMAIN_NAME, INCEPTION_DATA_DIR
-
-                  設定後は新しいターミナルを開いてください。
-
-                  Microsoft Visual Studio CodeとGoogle Chromeは、公開OVAには同梱していません。
-                  必要な場合はインターネット接続後、各ソフトウェアのライセンスを確認したうえで
-                  次のコマンドをターミナルから実行してください。
-
-                      NIXPKGS_ALLOW_UNFREE=1 nix profile install --impure nixpkgs#vscode
-                      NIXPKGS_ALLOW_UNFREE=1 nix profile install --impure nixpkgs#google-chrome
-
-                  インストール後にアプリ一覧へ表示されない場合は、一度ログアウトして
-                  ログインし直してください。
-                '';
-
-                xdg = {
-                  enable = true;
-                  userDirs = {
-                    enable = true;
-                    createDirectories = true;
-                  };
-                };
-
-                programs.home-manager.enable = true;
-              };
-            };
-          }
           ./nix/virtualbox.nix
         ];
       };
@@ -145,6 +49,14 @@
       packages.${system} = {
         default = vbox.config.system.build.image;
         vbox = vbox.config.system.build.image;
+        ova-import-check = pkgs.runCommand "inception-ova-import-check" {
+          nativeBuildInputs = [ pkgs.virtualbox ];
+        } ''
+          export HOME="$TMPDIR"
+          ova=$(find ${vbox.config.system.build.image} -type f -name '*.ova' -print -quit)
+          test -n "$ova"
+          VBoxManage import "$ova" --dry-run > "$out"
+        '';
       };
 
       checks.${system} = {
@@ -156,5 +68,6 @@
       };
 
       formatter.${system} = pkgs.nixfmt-tree;
+
     };
 }
