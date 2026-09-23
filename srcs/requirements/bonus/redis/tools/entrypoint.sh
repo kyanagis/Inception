200edad1a747411430d8f187405b874182c25f29
kyanagis <kyanagis42@gmail.com>
2026-09-23 20:36:33 +0000

    fix: narrow Redis ACL to explicit commands

--- srcs/requirements/bonus/redis/tools/entrypoint.sh
+++ srcs/requirements/bonus/redis/tools/entrypoint.sh
@@ -29,7 +29,7 @@ chmod 0700 /run/redis
 chown redis:redis /run/redis
 printf '%s\n' \
   'user default off' \
-  "user wordpress on >$password ~* +@read +@write +@connection +@scripting +ping +info +flushdb -flushall -config -acl -shutdown -module -replicaof -slaveof -save -bgsave" \
+  "user wordpress on >$password ~* -@all +ping +info +select +get +set +setex +psetex +del +unlink +exists +mget +mset +incr +incrby +decr +decrby +expire +pexpire +expireat +pexpireat +ttl +pttl +persist +scan +keys +type +flushdb +eval +evalsha +script|exists +script|load +client|setname +client|setinfo" \
   > /run/redis/users.acl
 chmod 0400 /run/redis/users.acl
 chown redis:redis /run/redis/users.acl
